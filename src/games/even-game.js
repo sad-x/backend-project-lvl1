c7084aad83889aea802bf8137f5cf87530ff8d59
sad-x <alex.shamess@ya.ru>
2022-02-01 18:07:51 +0300

    definitely last test

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,13 +1,11 @@
-
 import { getRandomInt } from '../random.js';
 import greet from '../cli.js';
 import play from '../index.js';
 
-
 const generateCorrectAnswer = (num) => {
   if (num % 2 === 0) return 'yes';
   return 'no';
-}
+};
 
 const generateQuestion = () => getRandomInt(1, 100);
 
@@ -15,5 +13,4 @@ export default () => {
   const name = greet();
   const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
   play(rules, generateQuestion, generateCorrectAnswer, name);
-}
-
+};
--- src/index.js
+++ src/index.js
@@ -4,24 +4,25 @@ const round = (generateQuestion, generateCorrectAnswer) => {
   const question = generateQuestion();
   const correctAnswer = generateCorrectAnswer(question);
   console.log(`Question: ${question}`);
-  const userAnswer = readlineSync.question('Your answer ')
+  const userAnswer = readlineSync.question('Your answer ');
+  // eslint-disable-next-line eqeqeq
   if (userAnswer != correctAnswer) {
     console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
     return false;
   }
   console.log('Correct!');
   return true;
-}
+};
 
 const play = (rules, generateQuestion, generateCorrectAnswer, name) => {
   console.log(rules);
   for (let i = 0; i < 3; i += 1) {
-    if(!round(generateQuestion, generateCorrectAnswer)) {
-      console.log(`Let's try again, ${name}!`)
+    if (!round(generateQuestion, generateCorrectAnswer)) {
+      console.log(`Let's try again, ${name}!`);
       return;
     }
   }
   console.log(`Congratulations, ${name}!`);
-}
+};
 
 export default play;
